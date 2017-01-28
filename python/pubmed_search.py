861c1c6c5e22bcf0a1ba884aec102a11cab4ab5a
Patrick Short <pjshort42@gmail.com>
2017-01-28 16:30:15 +0000

    added simple pubmed search with examples

---
+++ python/pubmed_search.py
@@ -0,0 +1,17 @@
+__author__ = 'pshort'
+
+from Bio import Entrez
+
+Entrez.email = "pjshort42@gmail.com"
+
+def EntrezSearch(rsid):
+	handle = Entrez.esearch(db="pmc", term=rsid)
+	record = Entrez.read(handle)
+	print(record["IdList"])
+	return(record["IdList"])
+
+if __name__ == "__main__":
+	EntrezSearch("rs7412")
+	EntrezSearch("rs11540652")
+	EntrezSearch("rs121912652")
+	EntrezSearch("rs121912653")
