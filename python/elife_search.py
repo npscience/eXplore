676eba6acf642758354563928d9133fda8c5d454
Patrick Short <pjshort42@gmail.com>
2017-01-28 17:21:10 +0000

    search eLife corpus for rsid

---
+++ python/elife_search.py
@@ -0,0 +1,33 @@
+__author__ = 'ps14'
+
+# search the eLife corpus for articles matching a particular rsid
+
+# curl -v "http://prod--gateway.elifesciences.org/search?for=dna" | python -m json.tool
+
+import subprocess
+
+import json
+
+
+def eLifeSearch(rsid):
+    cmd = "curl -v 'http://prod--gateway.elifesciences.org/search?for={}' | python -m json.tool".format(rsid)
+    print(cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+
+    # output is a json
+    results_dict = json.loads(output)
+
+    results = []
+
+    for item in results_dict['items']:
+        res_dict = {'rsid': rsid, 'type': item['type'], 'title': item["title"],
+                   'pdf': item["pdf"],
+                   'date_published': item["published"]}
+        results.append(res_dict)
+
+    return results
+
+
+if __name__ == "__main__":
+    eLifeSearch("rs1743292")
